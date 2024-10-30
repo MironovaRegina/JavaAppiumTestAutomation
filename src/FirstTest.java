9c6cf945ac9b3eb0656bdb754949800dc2573b94
MironovaRegina <k.regina4you@yandex.ru>
2024-10-30 21:25:38 +0300

    lesson4ex5

--- src/FirstTest.java
+++ src/FirstTest.java
@@ -1,5 +1,9 @@
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.android.options.UiAutomator2Options;
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.interactions.PointerInput;
+import org.openqa.selenium.interactions.Sequence;
+import java.util.Arrays;
 import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
@@ -8,9 +12,9 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
-
 import java.net.URL;
 import java.time.Duration;
+import java.util.Collections;
 
 public class FirstTest {
     private AndroidDriver driver;
@@ -24,7 +28,7 @@ public class FirstTest {
         options.setCapability("automationName","UiAutomator2");
         options.setCapability("appPackage","org.wikipedia");
         options.setCapability("appActivity",".main.MainActivity");
-        options.setApp(System.getProperty("user.dir") + "/apks/org.wikipedia.apk");
+        options.setApp(System.getProperty("user.dir") + "/apks/org.wikipedia_1.apk");
 
         driver = new AndroidDriver(
                 // The default URL in Appium 1 is http://127.0.0.1:4723/wd/hub
@@ -79,5 +83,45 @@ public class FirstTest {
         WebDriverWait wait = new WebDriverWait(driver, timeoutInSeconds);
         wait.withMessage(err_message+ "\n");
         return wait.until(ExpectedConditions.invisibilityOf(element));
+    }
+
+    @Test
+    public void swipeOnbordingTest(){
+        waitForElementPresent(By.xpath("//*[contains(@text, 'SKIP')]"),"элемент не найден",Duration.ofSeconds(5));
+        swipeRightToLeft();
+        WebElement title = waitForElementPresent(By.id("org.wikipedia:id/primaryTextView"),"элемент не найден",Duration.ofSeconds(5));
+        Assert.assertEquals(title.getAttribute("text"),"New ways to explore");
+
+        swipeRightToLeft();
+        title = waitForElementPresent(By.id("org.wikipedia:id/primaryTextView"),"элемент не найден",Duration.ofSeconds(5));
+        Assert.assertEquals(title.getAttribute("text"),"Reading lists with sync");
+
+        swipeRightToLeft();
+        title = waitForElementPresent(By.id("org.wikipedia:id/primaryTextView"),"элемент не найден",Duration.ofSeconds(5));
+        Assert.assertEquals(title.getAttribute("text"),"Send anonymous data");
+
+        waitForElementPresent(By.xpath("//*[contains(@text, 'GET STARTED')]"),"элемент не найден",Duration.ofSeconds(5)).click();
+
+        waitForElementPresent(By.xpath("//*[contains(@text, 'Search Wikipedia')]"),"элемент не найден",Duration.ofSeconds(5));
+    }
+
+    public void swipeRightToLeft() {
+        int duration = 800;
+        Dimension screenSize = driver.manage().window().getSize();
+
+        int startX = (int) (screenSize.width * 0.9);
+        int endX = (int) (screenSize.width * 0.1);
+        int y = screenSize.height / 2;
+
+        PointerInput finger = new PointerInput(PointerInput.Kind.TOUCH, "finger");
+        Sequence swipe = new Sequence(finger, 1);
+
+        swipe.addAction(finger.createPointerMove(Duration.ZERO, PointerInput.Origin.viewport(), startX, y));
+        swipe.addAction(finger.createPointerDown(PointerInput.MouseButton.LEFT.asArg()));
+
+        swipe.addAction(finger.createPointerMove(Duration.ofMillis(duration), PointerInput.Origin.viewport(), endX, y));
+        swipe.addAction(finger.createPointerUp(PointerInput.MouseButton.LEFT.asArg()));
+
+        driver.perform(Collections.singletonList(swipe));
     }
 }
